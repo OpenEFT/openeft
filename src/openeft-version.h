49683491c8c4aea5f9f6f1552440d961d7adccb3
Reza <reza.schadmani@openeft.org>
2018-04-04 20:48:07 +1000

    transaction format up

--- src/openeft-version.h
+++ src/openeft-version.h
@@ -9,7 +9,7 @@
 #define OPENEFT_VERSION_MAJOR       "0"
 #define OPENEFT_VERSION_MINOR       "1"
 #define OPENEFT_VERSION_PATCH       "0"
-#define OPENEFT_VERSION_REVISION    "4fba46639ab9"
+#define OPENEFT_VERSION_REVISION    "9a9d84af5ddb"
 
 #define OPENEFT_VERSION             "0.1.0"
-#define OPENEFT_NAME_VERSION        "openeft v0.1.0 (4fba46639ab9)"
+#define OPENEFT_NAME_VERSION        "openeft v0.1.0 (9a9d84af5ddb)"
