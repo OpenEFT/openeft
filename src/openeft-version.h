cd78d04a592125c996a3dba616343907bd2e6107
Reza <reza.schadmani@openeft.org>
2018-04-15 01:34:18 +1000

    cmake up

--- src/openeft-version.h
+++ src/openeft-version.h
@@ -9,7 +9,7 @@
 #define OPENEFT_VERSION_MAJOR       "0"
 #define OPENEFT_VERSION_MINOR       "1"
 #define OPENEFT_VERSION_PATCH       "0"
-#define OPENEFT_VERSION_REVISION    "0c0ee6ad06eb"
+#define OPENEFT_VERSION_REVISION    "8154934eebfb"
 
 #define OPENEFT_VERSION             "0.1.0"
-#define OPENEFT_NAME_VERSION        "openeft v0.1.0 (0c0ee6ad06eb)"
+#define OPENEFT_NAME_VERSION        "openeft v0.1.0 (8154934eebfb)"
