2daaf1b057db39c34755881a2f65086df9bf00ec
Reza <reza.schadmani@openeft.org>
2018-05-03 22:39:21 +1000

    blockchain protos added

--- src/openeft-version.h
+++ src/openeft-version.h
@@ -9,7 +9,7 @@
 #define OPENEFT_VERSION_MAJOR       "0"
 #define OPENEFT_VERSION_MINOR       "1"
 #define OPENEFT_VERSION_PATCH       "0"
-#define OPENEFT_VERSION_REVISION    "d315e1b92ab9"
+#define OPENEFT_VERSION_REVISION    "a35eaefd647d"
 
 #define OPENEFT_VERSION             "0.1.0"
-#define OPENEFT_NAME_VERSION        "openeft v0.1.0 (d315e1b92ab9)"
+#define OPENEFT_NAME_VERSION        "openeft v0.1.0 (a35eaefd647d)"
