2e5177b68b3526094548cbc1fb4bdbf9e01f7c7f
Reza <reza.schadmani@openeft.org>
2018-05-06 19:21:02 +1000

    eft node proto added

--- src/openeft-version.h
+++ src/openeft-version.h
@@ -9,7 +9,7 @@
 #define OPENEFT_VERSION_MAJOR       "0"
 #define OPENEFT_VERSION_MINOR       "1"
 #define OPENEFT_VERSION_PATCH       "0"
-#define OPENEFT_VERSION_REVISION    "2daaf1b057db"
+#define OPENEFT_VERSION_REVISION    "2d378cb1e949"
 
 #define OPENEFT_VERSION             "0.1.0"
-#define OPENEFT_NAME_VERSION        "openeft v0.1.0 (2daaf1b057db)"
+#define OPENEFT_NAME_VERSION        "openeft v0.1.0 (2d378cb1e949)"
