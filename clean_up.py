858a614b22e3cafbe21f1c6a142cd5b1cdf81a94
Reza <reza.schadmani@openeft.org>
2018-05-09 22:24:28 +1000

    README up

--- clean_up.py
+++ clean_up.py
@@ -1,3 +1,7 @@
+# Extend this file to cover clean ups for all temporary files.
+# Please pay extra attentions to test this file before any usage as you might
+# inadvertently remove source files. 
+
 import os
 
 ANSI_COLOR_GREEN = "\x1b[32m"
@@ -6,7 +10,7 @@ ANSI_COLOR_RESET = "\x1b[0m"
 def scandirs(path):
     for root, dirs, files in os.walk(path):
         for currentFile in files:
-            exts = ('.pb.h', '.pb.cc')
+            exts = ('.pb.h', '.pb.cc') # Only protobuf files
             if any(currentFile.lower().endswith(ext) for ext in exts):
                 os.remove(os.path.join(root, currentFile))
                 print "Removing " + ANSI_COLOR_GREEN + (os.path.join(root, currentFile)) + ANSI_COLOR_RESET
